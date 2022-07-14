682ac9b0a4cdd335770a3b88fc93c77750efe00e
Aleksey Khokhlow <lomtik25@gmail.com>
2022-07-14 18:30:05 +0300

    feat:done all tests

--- src/index.js
+++ src/index.js
@@ -38,7 +38,49 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const space = "**********"
+    let massWord = []
+    let i = 0;
+    let startPosition = 0
+    let position = expr.indexOf(space)
+    if(position === -1){
+        massWord[i] = expr
+    } else {
+        while(position !==-1){
+            massWord[i] = expr.slice(startPosition,position)
+            startPosition = position+10
+            position = expr.indexOf(space,position+1)
+            i++
+            if(position === -1){
+                massWord[i] = expr.slice(startPosition)
+            }
+        }
+    }
+    let result = []
+
+    for(let m = 0; m<massWord.length; m++){
+        let name =""
+        for(let z = 0; z<massWord[m].length;z = z+10){
+            let word = massWord[m].slice(z,z+10)
+            let index = word.indexOf(1)
+            word = word.slice(index)
+            let item = ""
+            for(let f = 0; f<word.length;f +=2){
+                if(word.slice(f,f+2) == '10'){
+                    item += "."
+                } else {
+                    item +="-"
+                }
+            }
+            name += MORSE_TABLE[item]
+        }
+        result[m] = name
+    }
+
+
+    
+    
+    return result.join(' ')
 }
 
 module.exports = {
